602cb83b064ffb03982437dce57215859a09d64c
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-10-16 15:45:04 +0200

    final version of modularity method and clustering algorithm

--- network_generator.py
+++ network_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import networkx as nx
+from tqdm import trange
 
 def ones_random_symm(dim, prob):
     rand = np.random.rand(dim,dim)
@@ -22,6 +23,7 @@ class UndirectedNetwork:
         if check_adjacency:
             self._check_adjacency()
         self.edge_list = self._edge_list()
+        self.edge_dict = self._edge_dict()
         self.netx = nx.Graph(self.A)
 
     def _check_adjacency(self):
@@ -36,6 +38,16 @@ class UndirectedNetwork:
         i_list, j_list = np.nonzero(np.triu(self.A))
         return list(zip(i_list,j_list))
 
+    def _edge_dict(self):
+        node1_list, node2_list = np.nonzero(self.A)
+        edge_dict = {}
+        for node1,node2 in zip(node1_list,node2_list):
+            edge_dict.setdefault(node1,[]).append(node2)
+        return edge_dict
+
+    def _edges_in(self, comm):
+        return ((u,v) for (u,v) in self.edge_list if u in comm and v in comm)
+
     def number_of_nodes(self):
         return self.A.shape[0]
 
@@ -46,10 +58,6 @@ class UndirectedNetwork:
         n = self.number_of_nodes()
         return {node : np.sum(self.A[node,:]) for node in range(n)}
 
-    def get_nodes_in_community(self, comm):
-        n = self.number_of_nodes()
-        return np.array([node for node in range(n) if self.node_community[node]==comm])
-
     def modularity_nx(self, partitions=None):
         communities = partitions if partitions is not None else self.partitions
         return nx.algorithms.community.modularity(self.netx, communities)
@@ -59,7 +67,69 @@ class UndirectedNetwork:
         self.number_of_partitions = len(communities)
         self.partitions = communities
 
-    def draw(self, ax, community_colors=False):
+    def modularity(self, partitions=None):
+        communities = partitions if partitions is not None else self.partitions
+        degree = self.degrees_of_nodes()
+        deg_sum = sum(degree.values())
+        m = deg_sum/2
+        norm = 1/(deg_sum**2)
+        def community_contribution(community):
+            comm = set(community)
+            L_c = sum(1 for u,v in self._edges_in(comm) if v in comm)
+            degree_sum = sum(degree[u] for u in comm)
+            return L_c/m - degree_sum*degree_sum*norm
+        return sum(map(community_contribution, communities))
+
+    def clustering(self):
+        n = self.number_of_nodes()
+        m = self.number_of_edges()
+        q0 = 1./(2.*m)
+        degrees = self.degrees_of_nodes()
+        k = np.array([degrees[i] for i in range(n)])
+        communities = {i : set([i]) for i in range(n)}
+        partitions = [[node for node in comm] for comm in communities.values()]
+        q = self.modularity(partitions)
+        a = np.array([k[i]*q0 for i in range(n)])
+        dq_matrix = np.zeros(shape=(n,n))
+        for i in range(n):
+            for j in [u for u in self.edge_dict[i]]:
+                if j != i:
+                    dq_matrix[i][j] = 2.*q0 - 2.*k[i]*k[j]*q0*q0
+
+        for _ in trange(n-1, desc="Clustering in progress"):
+            delta_q = np.max(dq_matrix)
+            if delta_q <= 0:
+                continue
+            i, j = np.where(dq_matrix == delta_q)
+            if isinstance(i, np.ndarray): i = i[0]
+            if isinstance(j, np.ndarray): j = j[0]
+            communities[j] = set(communities[i] | communities[j])
+            communities[i] = set([])
+            i_set = set(np.nonzero(dq_matrix[i,:])[0])
+            j_set = set(np.nonzero(dq_matrix[j,:])[0])
+            all_set = (i_set | j_set) - {i,j}
+            both_set = i_set & j_set
+            for k in all_set:
+                if k in both_set:
+                    dq_jk = dq_matrix[j][k] + dq_matrix[i][k]
+                elif k in j_set:
+                    dq_jk = dq_matrix[j][k] - 2.*a[i]*a[k]
+                else:
+                    dq_jk = dq_matrix[i][k] - 2.*a[j]*a[k]
+                dq_matrix[j][k] = dq_jk
+                dq_matrix[k][j] = dq_jk
+            dq_matrix[i,:] = 0
+            dq_matrix[:,i] = 0
+            a[j] += a[i]
+            a[i] = 0
+            q += delta_q
+
+        communities = [set([node for node in comm]) for comm in communities.values()]
+        communities = [c for c in communities if len(c) > 0]
+        self.number_of_partitions = len(communities)
+        self.partitions = communities
+
+    def draw(self, ax, community_colors=True):
         if not community_colors or self.number_of_partitions == 1:
             colors = "#1f78b4"
         else:
@@ -106,8 +176,7 @@ class Random_Blocks(UndirectedNetwork):
                 A[row:row+r, col:col+c] = ones_random_symm(r,p) if i==j else ones_random((r,c),p)
                 col += c
             row += r
-        A = np.triu(A) + np.triu(A).T
-        A -= np.diag(np.diag(A))
+        A = np.triu(A) + np.triu(A).T - np.diag(2*np.diag(A))
         UndirectedNetwork.__init__(self, A, check_adjacency)
         self.number_of_partitions = 1
         self.partitions = [range(self.number_of_nodes())]
@@ -132,29 +201,26 @@ if __name__ == "__main__":
             prob_matrix[i][j] = 0.005
     prob_matrix += prob_matrix.T
     for k in range(blocks):
-        prob_matrix[k][k] = 0.1
+        prob_matrix[k][k] = 0.2
 
     ti = time.time()
     random_blocks = Random_Blocks(blocks_sizes, prob_matrix,
                                  check_parameters=True, check_adjacency=True)
     tf = time.time()
-    print(f"\ntime for RBmodel generation = {tf-ti} sec")
+    print(f"\ntime for RBmodel generation = {tf-ti} sec", flush=True)
     fig, ax = plt.subplots(figsize=(8,8))
     random_blocks.show(ax)
     plt.show()
-
-    mod1 = random_blocks.modularity_nx()
-    print(f"\nmodularity Q(before clustering) = {mod1}")
-
     fig, ax = plt.subplots(figsize=(8,8))
-    random_blocks.draw(ax, community_colors=True)
-    plt.show()
+    random_blocks.draw(ax)
 
-    random_blocks.clustering_nx()
-
-    mod2 = random_blocks.modularity_nx()
-    print(f"\nmodularity Q(after clustering) = {mod2}")
-
+    mod1 = random_blocks.modularity()
+    print(f"\nmodularity Q(before clustering) = {mod1}", flush=True)
+    ti = time.time()
+    random_blocks.clustering()
+    tf = time.time()
+    print(f"\ntime for clustering = {tf-ti}", flush=True)
+    mod2 = random_blocks.modularity()
+    print(f"modularity Q(after clustering) = {mod2}", flush=True)
     fig, ax = plt.subplots(figsize=(8,8))
-    random_blocks.draw(ax, community_colors=True)
-    plt.show()
+    random_blocks.draw(ax)
