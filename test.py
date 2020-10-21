2ee781d8e08892ae86a85c8723cd5ca6bb820c94
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-10-21 15:50:54 +0200

    added test for new features

--- test.py
+++ test.py
@@ -1,32 +1,50 @@
 import numpy as np
 import pylab as plt
+import networkx as nx
 from time import time
 from randblocks import RandomBlocks
 
-blocks = 3
-n1=1000; n2=800; n3=900
-n = n1+n2+n3
-blocks_sizes = np.array([n1, n2, n3])
+def draw_nx(unet, ax, col_communities=False):
+        if unet.adjacency is None:
+            raise NotImplementedError("Draw method is not supported for very large networks")
+        netx = nx.Graph(unet.adjacency)
+        if col_communities and unet.number_of_partitions > 1:
+            p = unet.number_of_partitions
+            col = np.linspace(0,1,p)
+            colors = np.empty(unet.number_of_nodes)
+            for node in range(unet.number_of_nodes):
+                for i,p in enumerate(unet.partitions):
+                    if node in p:
+                        colors[node] = col[i]
+        else:
+            colors = "#1f78b4"
+        nx.draw(netx, ax=ax, width=0.2, node_size=20, node_color=colors, cmap="viridis")
+
+
+blocks = 4
+n1=180; n2=630; n3=390; n4=220
+blocks_sizes = np.array([n1,n2,n3,n4])
 prob_matrix = np.zeros(shape=(blocks,blocks))
 for i in range(blocks-1):
     for j in range(i+1, blocks):
-        prob_matrix[i][j] = 0.001
+        prob_matrix[i][j] = 0.0005
 prob_matrix += prob_matrix.T
 for k in range(blocks):
-    prob_matrix[k][k] = 0.01
+    prob_matrix[k][k] = 0.05
 
 ti = time()
 random_blocks = RandomBlocks(blocks_sizes, prob_matrix)
 tf = time()
 print(f"\ntime for RBmodel generation = {tf-ti} sec", flush=True)
-fig, ax = plt.subplots(figsize=(8,8))
-random_blocks.show(ax)
-plt.show()
 
 ti = time()
-random_blocks.clustering()
+mod = random_blocks.clustering(return_modularity=True, check_result=True)
 tf = time()
 print(f"\ntime for clustering = {tf-ti}", flush=True)
 fig, ax = plt.subplots(figsize=(8,8))
-random_blocks.show(ax, show_communities=True)
+ax.plot(mod)
+plt.show()
+fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(16,8))
+draw_nx(random_blocks, ax0, col_communities=True)
+random_blocks.show(ax1, show_communities=True)
 plt.show()
