100fca3a84819f0d65ec82508515d5e41fba6e15
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-10-21 10:30:15 +0200

    added method to check types and values of parameters

--- erdosrenyi.py
+++ erdosrenyi.py
@@ -1,18 +1,35 @@
 import numpy as np
 from unetwork import UndirectedNetwork
 
+
 class ErdosRenyi(UndirectedNetwork):
 
     def __init__(self, n, p, seed=None):
-        if seed is not None:
-            np.random.seed(seed)
+
+        self._check_parameters(n,p)
+
+        if seed is not None: np.random.seed(seed)
+
+        # build adjacency matrix A (symmetric and unweighted)
+        n = int(n)
         rand = np.random.rand(n,n)
         A = np.empty(shape=rand.shape, dtype=int)
         A = np.where(rand < p, 1, 0)
         self.adjacency = np.triu(A) + np.triu(A).T - np.diag(2*np.diag(A))
+
         m = int(np.sum(A)*0.5)
+
+        # build the edge dictionary from the adjacency matrix
         node1_list, node2_list = np.nonzero(A)
         edge_dict = {}
         for node1,node2 in zip(node1_list,node2_list):
             edge_dict.setdefault(node1,[]).append(node2)
+
         UndirectedNetwork.__init__(self,n,m,edge_dict, build_adjacency=True)
+
+    def _check_parameters(self, n, p):
+        if not isinstance(n, (int,np.integer)):
+            raise TypeError("n(number of nodes) must be an integer number")
+
+        if not 0 <= p <= 1:
+            raise ValueError("p(edge probability) must be in [0,1]")
--- randblocks.py
+++ randblocks.py
@@ -2,33 +2,57 @@ import numpy as np
 from unetwork import UndirectedNetwork
 from unetwork import ones_random_symm, ones_random
 
+
 class RandomBlocks(UndirectedNetwork):
 
-    def __init__(self, sizes, p_matrix, seed=None):
-        self.blocks = sizes.size
-        self.block_sizes = sizes
-        self.p = p_matrix
-        if seed is not None:
-            np.random.seed(seed)
-        n = np.sum(sizes)
+    def __init__(self, block_sizes, prob_matrix, seed=None):
+
+        if not isinstance(block_sizes, np.ndarray): block_sizes = np.array(block_sizes)
+        if not isinstance(prob_matrix, np.ndarray): prob_matrix = np.array(prob_matrix)
+
+        self._check_parameters(block_sizes, prob_matrix)
+        self.blocks = block_sizes.size
+
+        if seed is not None: np.random.seed(seed)
+
+        # build adjacency matrix A (symmetric and unweighted)
+        n = int(np.sum(block_sizes))
         A = np.zeros(shape=(n,n), dtype=int)
         for i in range(self.blocks):
-            row = col = np.sum(sizes[:i])
+            row = col = np.sum(block_sizes[:i])
             for j in range(i, self.blocks):
-                r = sizes[i]
-                c = sizes[j]
-                p = p_matrix[i][j]
+                r = block_sizes[i]
+                c = block_sizes[j]
+                p = prob_matrix[i][j]
                 A[row:row+r, col:col+c] = ones_random_symm(r,p) if i==j else ones_random((r,c),p)
                 col += c
             row += r
         A = np.triu(A) + np.triu(A).T - np.diag(2*np.diag(A))
         perms = np.random.permutation(range(n)) 
         self.adjacency = A[perms,:][:,perms]
+
+        m = int(np.sum(self.adjacency)*0.5)
+
         self.number_of_partitions = 1
         self.partitions = [range(n)]
-        m = int(np.sum(self.adjacency)*0.5)
+
+        # build the edge dictionary from the adjacency matrix
         node1_list, node2_list = np.nonzero(self.adjacency)
         edge_dict = {}
         for node1,node2 in zip(node1_list,node2_list):
             edge_dict.setdefault(node1,[]).append(node2)
+
         UndirectedNetwork.__init__(self,n,m,edge_dict, build_adjacency=True)
+
+    def _check_parameters(self, block_sizes, prob_matrix):
+        # check if all the elements in block_sizes are integers
+        if not np.all(np.mod(block_sizes,1) == 0):
+            raise TypeError("All the elements in block_sizes must be integer numbers")
+
+        # check if all the elements in prob_matrix are positive (probability)
+        if not np.all(np.where(prob_matrix>=0, True, False)):
+            raise ValueError("All the elements in prob_matrix must be positive numbers")
+
+        # check if all the elements in prob_matrix are equal or smaller than 1 (probability)
+        if not np.all(np.where(prob_matrix<=1, True, False)):
+            raise ValueError("All the elements in prob_matrix must be equal or smaller than 1")
