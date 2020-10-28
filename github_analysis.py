a5083ad3048ba0b4aac4add664af4167fd43dc3c
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-10-28 20:08:58 +0100

    added analysis of github communities

--- github_analysis.py
+++ github_analysis.py
@@ -1,17 +1,8 @@
 from unetwork import UndirectedNetwork
-
-def count_connections(net):
-    # counts the connections in net starting from edge_dict (each edge twice)
-    edges = 0
-    for node in net.edge_dict:
-        edges += len(net.edge_dict[node])
-    return edges
+from visualization import draw_communities_graph, draw_communities_barplot
 
 
 github_net = UndirectedNetwork.fromfile(filename="data/github.txt")
-
-assert github_net.number_of_nodes == len(github_net.edge_dict)
-assert github_net.number_of_edges*2 == count_connections(github_net)
 
 mod1 = github_net.modularity()
 print(f"number of communities before clustering = {github_net.number_of_communities}")
@@ -20,3 +11,8 @@ github_net.clustering()
 mod2 = github_net.modularity()
 print(f"number of communities after clustering = {github_net.number_of_communities}")
 print(f"modularity after clustering = {mod2}")
+
+
+draw_communities_graph(github_net, min_size=10, scale="root")
+
+draw_communities_barplot(github_net, min_size=10, scale="log")
