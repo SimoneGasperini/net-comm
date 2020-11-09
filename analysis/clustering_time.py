7245876dddac414d2908535dd79985356e33a844
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-11-09 15:51:32 +0100

    added confidence band in clustering time plots

--- analysis/clustering_time.py
+++ analysis/clustering_time.py
@@ -8,13 +8,14 @@ from randblocks import RandomBlocks
 
 
 
+num_simulations = 10 # number of repetitions of the same clustering
+sigma_conf = 5. # sigma confidence level shown in the plot
+
 blocks = 2
-n_i = 200 # initial size of each block
-n_f = 2000 # final size of each block
+n_i = 300 # initial size of each block
+n_f = 1000 # final size of each block
 num_points = 10
 delta_n = int((n_f-n_i)/(num_points-1))
-
-blocks_sizes = np.array([n_i, n_i])
 
 prob_matrix = np.zeros(shape=(blocks,blocks))
 for i in range(blocks-1):
@@ -25,47 +26,58 @@ for k in range(blocks):
     prob_matrix[k][k] = 0.1
 
 
-time_seq = np.empty(num_points)
+times = np.empty(shape=(num_simulations,num_points))
 nodes_seq = np.empty(num_points)
 edges_seq = np.empty(num_points)
 
-print(f"Clustering adding each time {delta_n} nodes for each one of the {blocks} blocks -->", flush=True)
-for i in range(num_points):
+for i in range(num_simulations):
 
-    random_blocks = RandomBlocks(blocks_sizes, prob_matrix)
+    print(f"\nSimulation {i+1}/{num_simulations}")
+    blocks_sizes = np.array([n_i, n_i])
 
-    ti = time()
-    random_blocks.clustering(check_result=True)
-    tf = time()
-    time_seq[i] = tf-ti
+    for j in range(num_points):
 
-    nodes_seq[i] = random_blocks.number_of_nodes
-    edges_seq[i] = random_blocks.number_of_edges
+        random_blocks = RandomBlocks(blocks_sizes, prob_matrix)
 
-    blocks_sizes += delta_n
+        ti = time()
+        random_blocks.clustering(check_result=True)
+        tf = time()
+        times[i][j] = tf-ti
+
+        if i == 0:
+            nodes_seq[j] = random_blocks.number_of_nodes
+            edges_seq[j] = random_blocks.number_of_edges
+
+        blocks_sizes += delta_n
 
 
 plt.style.use('seaborn-paper')
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize=(12,8))
 
-ax0.set_title("time trend with nodes")
-ax0.plot(nodes_seq, time_seq)
-ax0.set_xlabel("number of nodes")
-ax0.set_ylabel("clustering time")
-ax0.spines['right'].set_visible(False)
-ax0.spines['top'].set_visible(False)
-ax0.xaxis.set_ticks_position('bottom')
-ax0.yaxis.set_ticks_position('left')
+mean_time = np.mean(times, axis=0)
+std_time = np.std(times, axis=0)
 
-ax1.set_title("time trend with edges")
-ax1.plot(edges_seq, time_seq, color="orange")
-ax1.set_xlabel("number of edges")
-ax1.set_ylabel("clustering time")
-ax1.spines['right'].set_visible(False)
-ax1.spines['top'].set_visible(False)
-ax1.xaxis.set_ticks_position('bottom')
-ax1.yaxis.set_ticks_position('left')
+fig, ax = plt.subplots(figsize=(8,8))
+ax.set_title("time trend with nodes")
+ax.plot(nodes_seq, mean_time)
+ci = sigma_conf * std_time
+ax.fill_between(nodes_seq, (mean_time-ci), (mean_time+ci), alpha=.2)
+ax.set_xlabel("number of nodes")
+ax.set_ylabel("clustering time")
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+plt.show()
 
-
-plt.suptitle(f"Clustering adding each time {delta_n} nodes for each one of the {blocks} blocks")
+fig, ax = plt.subplots(figsize=(8,8))
+ax.set_title("time trend with edges")
+ax.plot(edges_seq, mean_time, color="orange")
+ci = sigma_conf * std_time
+ax.fill_between(edges_seq, (mean_time-ci), (mean_time+ci), color="orange", alpha=.2)
+ax.set_xlabel("number of edges")
+ax.set_ylabel("clustering time")
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
 plt.show()
