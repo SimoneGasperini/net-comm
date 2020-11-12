e03cb6afd97e8d6a4270cf5f24ef31abff8f60de
giuseppefilitto <giuseppefilitto@gmail.com>
2020-11-12 14:23:07 +0100

    added fit

--- analysis/clustering_time.py
+++ analysis/clustering_time.py
@@ -6,8 +6,16 @@ import sys
 sys.path.insert(0,"..")
 from randblocks import RandomBlocks
 
+from scipy.optimize import curve_fit
 
-num_simulations = 10 # number of repetitions of the same clustering
+def f_1(x, intercept, slope):
+       return intercept + slope*x
+
+
+def f_2(x, p0, p1, p2):
+       return p0 + p1*x + p2*(x**2)
+
+num_simulations = 10# number of repetitions of the same clustering
 sigma_conf = 5. # sigma confidence level shown in the plot
 
 blocks = 2
@@ -55,10 +63,22 @@ plt.style.use('seaborn-paper')
 mean_time = np.mean(times, axis=0)
 std_time = np.std(times, axis=0)
 
+#fit
+x = nodes_seq
+y = mean_time
+
+popt, pcov = curve_fit(f_2, x, y)
+
+p0 = popt[0]
+p1 = popt[1]
+p2 = popt[2]
+
 fig, ax = plt.subplots(figsize=(8,8))
-ax.plot(nodes_seq, mean_time)
+ax.plot(nodes_seq, mean_time, label='data')
+#fit_plot
+ax.plot(nodes_seq, f_2(nodes_seq, p0, p1, p2), 'r--', label='fit: p0=%5.3f, p1=%5.3f, p2=%5.3f' % tuple(popt))
 ci = sigma_conf * std_time
-ax.fill_between(nodes_seq, (mean_time-ci), (mean_time+ci), alpha=.2)
+ax.fill_between(nodes_seq, (mean_time-ci), (mean_time+ci), alpha=.2, label='c.i.')
 ax.set_xlabel("number of nodes", fontsize=16)
 ax.set_ylabel("time [s]", fontsize=16)
 ax.spines['right'].set_visible(False)
@@ -69,12 +89,24 @@ for tx in ax.xaxis.get_major_ticks():
     tx.label.set_fontsize(12)
 for ty in ax.yaxis.get_major_ticks():
     ty.label.set_fontsize(12)
+plt.legend(loc="best")
 plt.show()
 
+
+#fit
+x = edges_seq
+y = mean_time
+
+popt, pcov = curve_fit(f_1, x, y)
+intercept = popt[0]
+slope = popt[1]
+
 fig, ax = plt.subplots(figsize=(8,8))
-ax.plot(edges_seq, mean_time, color="orange")
+ax.plot(edges_seq, mean_time, color="orange", label='data')
+#fit_plot
+ax.plot(edges_seq, f_1(edges_seq, intercept, slope), 'g--', label='fit: intercept=%5.4f, slope=%5.4f'  % (intercept, slope))
 ci = sigma_conf * std_time
-ax.fill_between(edges_seq, (mean_time-ci), (mean_time+ci), color="orange", alpha=.2)
+ax.fill_between(edges_seq, (mean_time-ci), (mean_time+ci), color="orange", alpha=.2, label='c.i.')
 ax.set_xlabel("number of edges", fontsize=16)
 ax.set_ylabel("time [s]", fontsize=16)
 ax.spines['right'].set_visible(False)
@@ -85,4 +117,5 @@ for tx in ax.xaxis.get_major_ticks():
     tx.label.set_fontsize(12)
 for ty in ax.yaxis.get_major_ticks():
     ty.label.set_fontsize(12)
+plt.legend(loc="best")
 plt.show()
