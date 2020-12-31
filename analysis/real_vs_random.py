81783bc4d63728c82ef23583fb737f9e7dcea462
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2020-12-31 14:49:14 +0100

    minor fix

--- analysis/real_vs_random.py
+++ analysis/real_vs_random.py
@@ -2,59 +2,62 @@ import sys
 sys.path.append("..")
 
 from model.unet import UndirectedNetwork
-from model.erdosrenyi import ErdosRenyi
+from model.randomnet import RandomNetwork
 
-file = "../data/HEP_Collaboration.txt"
+
+file = '../data/HEP_Collaboration.txt'
 
 real_network = UndirectedNetwork.fromfile(file)
 
 part = real_network.clustering()
-mod = real_network.modularity(part)
-print(f"\nmod = {mod}", flush=True)
+mod_real = real_network.modularity(part)
+print(f'\nmod_real = {mod_real}\n', flush=True)
 
 n = real_network.number_of_nodes
 m = real_network.number_of_edges
-p = (2. * m) / n**2
 
-random_network = ErdosRenyi(n, p)
+random_network = RandomNetwork(n, m)
 
-part_er = random_network.clustering()
-mod_er = random_network.modularity(part_er)
-print(f"\nmod_random = {mod_er}", flush=True)
+part = random_network.clustering()
+mod_rand = random_network.modularity(part)
+print(f'\nmod_random = {mod_rand}\n', flush=True)
+
 
 #%%
 # using networkx
-import networkx as nx
+
+from networkx.algorithms.bipartite.edgelist import read_edgelist
 from networkx.generators.random_graphs import erdos_renyi_graph
 from networkx.algorithms.community.modularity_max import greedy_modularity_communities
 from networkx.algorithms.community.quality import modularity
 
-netx = nx.read_edgelist(file)
+netx = read_edgelist(file)
 
 partx = greedy_modularity_communities(netx)
-modx = modularity(G=netx, communities=partx)
-print(f"\nmod_nx = {modx}")
+modx_real = modularity(G=netx, communities=partx)
+print(f'\nmod_real_nx = {modx_real}')
 
-random_networkx = erdos_renyi_graph(n=n, p=p)
+p = (2. * m) / n**2
+netx_er = erdos_renyi_graph(n=n, p=p)
 
-partx_er = greedy_modularity_communities(random_networkx)
-modx_er = modularity(G=random_networkx, communities=partx_er)
-print(f"\nmod_nx_random = {modx_er}")
+partx = greedy_modularity_communities(netx_er)
+modx_rand = modularity(G=netx_er, communities=partx)
+print(f'\nmod_random_nx = {modx_rand}')
+
 
 #%%
-
 # data plotting
 
 import pandas as pd
 import seaborn as sns
 import pylab as plt
 
-data = {'Networks': ['Real network', 'Random', 'Real Network  ', 'Random '],
-        'Modularity': [mod, mod_er, modx, modx_er],
+data = {'Networks': ['Real network', 'Random', 'Real Network', 'Random'],
+        'Modularity': [mod_real, mod_rand, modx_real, modx_rand],
         'Class': ['Our Algorithm', 'Our Algorithm', 'NetworkX', 'NetworkX']
         }
 
 df = pd.DataFrame(data=data)
 
-g = sns.catplot(x='Networks', y='Modularity', kind='bar', hue="Class", dodge=False, data=df, height=5)
+g = sns.catplot(x='Networks', y='Modularity', kind='bar', hue='Class', dodge=False, data=df, height=5)
 plt.show()
