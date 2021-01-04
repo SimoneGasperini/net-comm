b4a222604d8c675e7778e6f3c48c7501192ac964
SimoneGasperini <simone.gasperini2@studio.unibo.it>
2021-01-04 10:29:51 +0100

    minor fix in example

--- run_example.py
+++ run_example.py
@@ -7,9 +7,9 @@ from model.visualization import draw
 
 
 blocks = 4
-blocks_sizes = np.array([np.random.randint(200,600) for i in range(blocks)])
-prob_within = 0.1
-prob_between = 0.002
+blocks_sizes = np.array([np.random.randint(100,300) for i in range(blocks)])
+prob_within = 0.08
+prob_between = 0.0005
 
 prob_matrix = np.empty(shape=(blocks,blocks))
 
@@ -17,7 +17,7 @@ for i in range(blocks):
     for j in range(i, blocks):
         prob_matrix[i][j] = prob_matrix[j][i] = prob_within if i == j else prob_between
 
-prob_matrix[0,0] = 0.18
+prob_matrix[0,0] = 0.1
 prob_matrix[2,2] = 0.25
 
 
@@ -29,11 +29,11 @@ print(f"\nModularity = {modularity}")
 
 
 fig1, ax1 = plt.subplots(figsize=(8,8))
-draw(er_blocks, partition, ax=ax1)
+draw(er_blocks, partition, ax=ax1, cmap='plasma')
 plt.show()
 
 fig2, ax2 = plt.subplots(figsize=(8,8))
-er_blocks.show(partition, ax=ax2, cmap='binary')
+er_blocks.show(partition, ax=ax2, cmap='plasma')
 plt.show()
 
 
